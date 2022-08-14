63d3b214f42339116a559881998b041971d9d4d1
codetariat <71725599+codetariat@users.noreply.github.com>
2022-08-14 14:31:19 +0200

    Add logos, add folder transpilation support and expand readme

--- src/roblox_py/transpiler/transpiler.py
+++ src/roblox_py/transpiler/transpiler.py
@@ -1,12 +1,14 @@
+from unittest import result
 from ..util import transpilation as transpilation_util;
+from ..util import strings as string_util;
 
 import os
 import re
 import ast
 
 def transpileFile(filePath: str) -> dict[str, str]:
-    result = None;
-    error = None;
+    result: any = None;
+    error: str | None = None;
 
     # Try to read the file given to us
     try:
@@ -16,10 +18,10 @@ def transpileFile(filePath: str) -> dict[str, str]:
         error = filePath + " is not a valid path: " + str(e);
     
     # Get non-existent file out of the way
-    if error != None: return { "result": None, "error": error };
+    if error != None: return { "error": error };
 
     # Get non-strings out of the way
-    if not isinstance(result, str): return { "result": None, "error": "File is not a string" };
+    if not isinstance(result, str): return { "error": "File is valid" };
 
     # Try ast.parse(ast.unparse(result))
     try:
@@ -29,7 +31,7 @@ def transpileFile(filePath: str) -> dict[str, str]:
         error = "Error parsing file: " + str(e);
 
     # Get errored file out of the way
-    if error != None: return { "result": None, "error": error };
+    if error != None: return { "error": error };
 
     # Turn every ":" into a ":" plus whiteline
     result = re.sub(r":", r":\n", result);
@@ -49,28 +51,54 @@ def transpileFile(filePath: str) -> dict[str, str]:
 
     # Deal with identation and replacing def with function, elif with else if
     transpilation_util.deal_with_indentation(lines);
-    
-    return { "result": "\n".join(lines), "error": error };
 
-def transpileFolder(folderPath: str) -> dict[str, str]:
+    result = "--Transpiled with roblox-py" + "\n".join(lines)
+    
+    return { "result": result };
+
+def transpileFolder(folderOrigin: str, folderDestination: str) -> dict[str, str]:
     results = {};
     errors = {};
 
-    for root, dirs, files in os.walk(folderPath, topdown=False):
+    for root, dirs, files in os.walk(folderOrigin, topdown=False):
         # Loop through directories and files
         for name in files:
             # Skip non-".py" files
             if not name.endswith(".py"): continue;
             
+            # Get full name
+            fullName = os.path.join(root, name);
+
             # Transpile and add the result to result[name]
-            transpilation = transpileFile(os.path.join(root, name))
+            transpilation = transpileFile(fullName)
 
             if "error" in transpilation:
-                errors[len(errors)] = transpilation["error"];
+                errors[fullName] = transpilation["error"];
 
             if "result" in transpilation:
-                print(transpilation["result"])
-                results[len(results)] = transpilation["result"];
+                results[fullName] = transpilation["result"];
 
+    # Empty the destination FOLDER
+    for root, dirs, files in os.walk(folderDestination, topdown=False):
+        for name in files:
+            os.remove(os.path.join(root, name));
+        for name in dirs:
+            os.rmdir(os.path.join(root, name));
+
+    # Get folderDestination full name
+    folderDestinationFullName = os.path.join(os.getcwd(), folderDestination);
+    
+    # Create and write the results to the destination folder
+    for fullName in results:
+        nameWithoutRoot = fullName.replace(folderOrigin + "\\", "");
+
+        newFileName = os.path.join(folderDestinationFullName, nameWithoutRoot)
+
+        # Replace the last .py with .lua
+        newFileName = string_util.replace_reverse(newFileName, ".py", ".lua", 1);
+
+        os.makedirs(os.path.dirname(newFileName), exist_ok=True)
+        with open(newFileName, "w") as f:
+            f.write(results[fullName])
 
     return {"results": results, "errors": errors};
